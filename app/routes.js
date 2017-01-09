156825c83e00f0ed0445c5bd3cf856dfcacd21c7
tokdaniel <tokdaniel4@gmail.com>
2017-01-09 19:21:23 +0100

    app routes through content component

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+
 import App from './components/app/App.jsx';
-import Example from './components/example/Example.jsx';
+import Example from './components/example/Example.jsx'
 
 export default (
 	<Route path="/" component={App}>
-		<IndexRoute component={Example} />
+		<IndexRoute />
 	</Route>
 );
