2d6c3b4396b9dcd7db7f7de6df9720ac3135a05f
tokdaniel <tokdaniel4@gmail.com>
2017-01-09 19:23:13 +0100

    webpack config slight mod

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
     entry: {
         app: "./app/index.js",
         vendor: [
@@ -43,13 +42,9 @@ module.exports = {
                 loader: 'html-loader'
             },
             {
-                test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff'
+                test: /\.(png|jpg|woff)$/,
+                loader: 'url-loader'
             },
-            {
-                test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/,
-                loader: 'file'
-            }
         ]
     },
     plugins: [
