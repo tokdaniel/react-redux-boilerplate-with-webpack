c9364eb2491313ece90e16e2475a72356d3598a5
ADS\u561478 <daniel.tok@dlh.de>
2017-01-10 14:14:54 +0100

    dunno what did i modify -> commit anyways

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app/App.jsx';
-import Example from './components/example/Example.jsx'
 
 export default (
-	<Route path="/" component={App}>
-		<IndexRoute />
-	</Route>
+    <Route path="/" component={App}>
+        <IndexRoute />
+    </Route>
 );
