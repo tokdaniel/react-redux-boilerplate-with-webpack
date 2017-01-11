94ff52c04aae6ee9a6a1870f06885670629aeef7
tokdaniel <tokdaniel4@gmail.com>
2017-01-11 15:48:15 +0100

    added config folder and index.js for import

---
+++ src/config/sidebar-menu.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        name: 'First Menu',
+        url: '/Login',
+        icon: 'fa fa-lg fa-user',
+    },
+    {
+        name: 'Second Menu',
+        url: '/Second',
+        icon: 'fa fa-chevron-right',
+        children: {}
+    },
+    {
+        name: 'Third Menu',
+        url: '/Third',
+    },
+    {
+        name: 'Fourth Menu',
+        url: '/Fourth',
+    },
+]
