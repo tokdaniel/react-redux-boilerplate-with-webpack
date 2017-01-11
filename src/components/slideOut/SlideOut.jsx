55e9ce5c2852d4d9812b7072aa84f91c5dd86d5f
tokdaniel <tokdaniel4@gmail.com>
2017-01-11 15:47:07 +0100

    added menu rendering on Slideout from config

--- src/components/slideOut/SlideOut.jsx
+++ src/components/slideOut/SlideOut.jsx
@@ -1,14 +1,21 @@
 import React, {Component, PropTypes} from 'react';
 import './SlideOut.scss';
+import {Link} from 'react-router';
+import {SidebarMenu} from "config";
 
 
 export default class SlideOut extends Component {
 
     render() {
+
         return (
             <nav>
                 <ul>
-                    <li>LOGIN / SIGN-UP</li>
+                    {
+                        SidebarMenu.map((item) =>{
+                            return <li key={item.url}><Link to={item.url}><p>{item.name}</p><i className={item.icon}></i></Link></li>
+                        })
+                    }
                 </ul>
             </nav>
         );
